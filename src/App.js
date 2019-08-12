866d635d26b0821ae8a2d3a106bb7b99658bf261
jaypal kadam <jaypal.kadam@koruux.com>
2019-08-12 18:37:00 +0530

    app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React ,{ Component} from 'react';
+import React ,{useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person'
@@ -9,22 +9,64 @@ import Person from './Person/Person'
 // }
 
 
-class App extends Component{
-  render(){
+const App = props => {
+
+   const [PersonState, setPersonSate] = useState({
+
+      persons:[
+        {name:'max',age:27},
+        {name:'royal',age:87},
+        {name:'gogo',age:47}
+      ],
+      otherState:'some other value'
+  });
+
+  const switchNameHandler = () => {
+      //console.log('Was clicked');
+      setPersonSate({
+        persons:[
+          {name:'maxmilliam',age:27},
+          {name:'royal',age:87},
+          {name:'gogo',age:38}
+        ]
+      })    
+    }
+
     return(
        <div className="app">
          <h1>this is the dummy class</h1>
          <p> This is really working nice</p>
-         <Person name="jhon" age="25"></Person>
-         <Person name="Max" age="5">
+         <button onClick={switchNameHandler} >Switch Name</button>
+         <Person name={PersonState.persons[0].name} age={PersonState.persons[0].age}></Person>
+         <Person name= {PersonState.persons[1].name} age={PersonState.persons[1].age}>
            This is the my dynamic Component
          </Person>
-         <Person name="apple" age="10"></Person>
+         <Person name={PersonState.persons[2].name} age={PersonState.persons[2].age}></Person>
        </div>
-
     )
       //return React.createElement('div', {className:'app'}, React.createElement('h1',null,'This work now automatically'))
   }
-}
+
 
 export default App;
+
+
+// state = {
+//   persons:[
+//     {name:'max',age:27},
+//     {name:'royal',age:87},
+//     {name:'gogo',age:47}
+//   ],
+//   otherState:'some other value'
+// // }
+// switchNameHandler = () => {
+//   //console.log('Was clicked');
+//   this.setState({
+//     persons:[
+//       {name:'maxmilliam',age:27},
+//       {name:'royal',age:87},
+//       {name:'gogo',age:38}
+//     ]
+//   })
+
+// }
